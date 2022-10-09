1da8af7cd180b442ef70d6ba51d58f46ddb5bf2f
Josephine <sosodjibrildiop@gmail.com>
2022-10-09 22:58:42 +0000

    2nd push

---
+++ page_présentation.php
@@ -0,0 +1,172 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page_présentation</title>
+    <link rel="stylesheet" href="mon_style.css">
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  
+<div class="container-fluid">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-white">
+                    <img src="image/logo2.png" alt="monlogo" class="logo">
+                    <a class="navbar-brand" href="#">Ecole de la Reussite</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav link">
+                 <li class="nav-item active">
+                    <a class="nav-link" href="page_accueil.php">ACCUEIL<span class="sr-only">(current)</span></a>
+                 </li>
+                 <li class="nav-item">
+                    <a class="nav-link" href="page_présentation.php">PRESENTATIONS</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="page_pedagogie.php">PEDAGOGIE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">SERVICES</a>
+                </li>
+                <li class="nav-item">
+                     <a class="nav-link mr-5" href="#">CONTACT</a>
+                </li>
+                 <div class="inscrire mr-5">  
+                <li class="nav-item dropdown ">
+                 <a class="nav-link dropdown-toggle  "  href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                S'INSCRIRE
+                </a>
+                <div class="dropdown-menu " aria-labelledby="navbarDropdownMenuLink">
+                <a class="dropdown-item" href="#">Eleve</a>
+                <a class="dropdown-item" href="#">Employer</a>
+              
+                </div>
+                </li>
+                </div>
+            <div class="connecte">    
+                  <li class="nav-item">
+                    <a class="nav-link " href="#">SE CONNECTER</a>
+                  </li>
+            </div>  
+      
+            </ul>
+            </div>
+            </nav>
+         </div>
+     </div>
+  </div>
+       <!-- Section 2 -->
+       <div class="container-fluid ">
+            
+          <div class="row sec2">
+          <div class="col-lg-12 col-md-6 col-sm-12">
+          <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-info text-center"><strong>A propos De l'école</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit.
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque 
+            Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+            </div>
+            </div>
+          <!-- <div class="col-lg-6 col-md-6 col-sm-12 imag">
+                <img  src="image/img2.jpg " alt="monImage">
+          </div> -->
+                </div>
+            </div>
+       <!-- </div> -->
+       <!-- End Section 2 -->
+        <!-- Section 3 -->
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-lg-12 col-md-6 col-sm-12 text-center">
+                <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-info"><strong>Transport</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor 
+            sit amet consectetur adipisicisit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <img src="image/bus2.jpg" alt="iiiiii"><br>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+                </div>
+
+             
+                </div>
+
+                
+            </div>
+            <div class="container-fluid">
+           <div class="row">
+           <div class="col-lg-12 col-md-6 col-sm-12 text-center">
+                <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-info"><strong>Infirmerie</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisici
+            sit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <img src="image/santé.jpg" alt="santé"><br>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+    <div class="col-lg-12 col-md-6 col-sm-12 text-center ">
+                <div class="card mt-5" >
+            <div class="card-body">
+            <h5 class="card-title text-info"><strong>Restauration</strong></h5>
+            <h6 class="card-subtitle mb-2 text-muted"></h6>
+            <p class="card-text">Lorem ipsum dolor
+            sit amet consectetur adipisici sit amet consectetur adipisicing elit. Voluptas cumque laboriosam sapiente beatae enim odio praesentium nam temporibus eius, neque autem reprehenderit dolore consequuntur, officia ullam possimus quos deleniti provident.</p>
+            <img src="image/restau.jpg" alt="restau"><br>
+            <a href="#" class="card-link " ><button class="btn text-dark">Savoir plus</button></a>
+            </div>
+            </div>
+    </div>
+               
+           </div> 
+           </div>
+        </div>
+        <!-- End Section3 -->
+        <!-- Footer -->
+
+      
+        <div class="row text-center ">
+<div class="col-12 bg-dark text-white w-100">
+    <footer>
+        <p>By Elhadji Souka Diouf,Mame Fatima Ndiaye,Cheikh Diouf et Joséphine Diop</p>
+    </footer>
+</div>
+</div>
+  <!-- End Footer -->
+
+    </div>
+
+    </div>
+    </div>
+</body>
+</html>
