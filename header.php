cc30fb1ca8dca1d26d582a8292b6331f54d3030f
pape souka <papesoukad@gmail.com>
2022-10-13 17:47:39 +0200

    phase 1

--- header.php
+++ header.php
@@ -1,40 +1,3 @@
-<!-- <!doctype html>
-<html lang="en">
-  <head>
-    <title>Hello, world!</title>
-     Required meta tags -->
-  <!--   <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
- -->
-    <!-- Bootstrap CSS -->
-   <!--  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-  </head>
-  <body  style="background-color: #1E434Eff;">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="button-admin.php">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="fichier.php">Acceuil <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-       <a class="nav-link"  class="btn btn-info" href="create.php">Ajouter</a>
-      </li>
-      
-      
-    </ul>
-  </div> -->
-<!-- </nav> --> 
- 
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -108,12 +71,3 @@
   </div>
 
 
-
-
-  <?php
-  include("codejs.php");
-  ?>
-</body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> 
